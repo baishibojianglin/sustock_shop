f9bdcd903072b515af08c62eee081c5d0f36e17e
Greetme <1980900198@qq.com>
2020-09-27 19:20:41 +0800

    main.ts

--- sustock_shop_admin_element/src/main.ts
+++ sustock_shop_admin_element/src/main.ts
@@ -11,9 +11,8 @@ import 'element-ui/lib/theme-chalk/index.css'
 // 公用配置
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios  //ajax插件
-// Vue.prototype.$serverUrl = 'http://media.sustock.local/index.php/' // 测试接口域名（本地）
-// Vue.prototype.$url='http://media.dilinsat.com/index.php/'  //测试接口域名
-Vue.prototype.$url='https://media.sustock.net/index.php/'  //正式接口域名
+// Vue.prototype.$url=''  //测试接口域名
+Vue.prototype.$url=''  //正式接口域名
 axios.defaults.withCredentials=true  //解决跨域后保持相同session(允许ajax携带cook)
 Vue.use(ElementUI)
 
