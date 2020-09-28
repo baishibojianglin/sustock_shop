ed32f0e97a2fd2a7e2b217f86d1e0ea3d5117ae1
Sunnee <2046829840@qq.com>
2020-09-28 15:04:22 +0800

    commit database

--- sustock_shop_api/application/database.php
+++ sustock_shop_api/application/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => '',
+    'database'        => 'sustock_shopmedia_test',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'sustockshopmediatest',
     // 密码
-    'password'        => '',
+    'password'        => 'sustockshopmediatest20200517',
     // 端口
     'hostport'        => '',
     // 连接dsn
